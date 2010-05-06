d5339786ff617be976bd0b120b4a27aef763a325
tdiesler <thomas.diesler@jboss.com>
2010-05-06 09:21:07 +0200

    Add logging for 'git submodule status'

--- hudson/hudson-home/command.sh
+++ hudson/hudson-home/command.sh
@@ -15,42 +15,42 @@ case "$CONTAINER" in
     SERVER_NAME=default
     JBOSS_BUILD=jboss-5.0.1.GA
     JBOSS_ZIP=$HUDSON_HOME/../jboss/$JBOSS_BUILD.zip
-	RUNTIME_HOME=$WORKSPACE/$JBOSS_BUILD
-	RUNTIME_LOG=$RUNTIME_HOME/server/$SERVER_NAME/log/server.log
-    rm -rf $RUNTIME_HOME; unzip -q $JBOSS_ZIP -d $WORKSPACE  
-    cp --backup $HUDSONBIN/run-with-pid.sh $RUNTIME_HOME/bin/run.sh
+	CONTAINER_HOME=$WORKSPACE/$JBOSS_BUILD
+	CONTAINER_LOG=$CONTAINER_HOME/server/$SERVER_NAME/log/server.log
+    rm -rf $CONTAINER_HOME; unzip -q $JBOSS_ZIP -d $WORKSPACE  
+    cp --backup $HUDSONBIN/run-with-pid.sh $CONTAINER_HOME/bin/run.sh
   ;;
   'jboss510')
     SERVER_NAME=default
     JBOSS_BUILD=jboss-5.1.0.GA
     JBOSS_ZIP=$HUDSON_HOME/../jboss/$JBOSS_BUILD.zip
-	RUNTIME_HOME=$WORKSPACE/$JBOSS_BUILD
-	RUNTIME_LOG=$RUNTIME_HOME/server/$SERVER_NAME/log/server.log
-    rm -rf $RUNTIME_HOME; unzip -q $JBOSS_ZIP -d $WORKSPACE  
-    cp --backup $HUDSONBIN/run-with-pid.sh $RUNTIME_HOME/bin/run.sh
+	CONTAINER_HOME=$WORKSPACE/$JBOSS_BUILD
+	CONTAINER_LOG=$CONTAINER_HOME/server/$SERVER_NAME/log/server.log
+    rm -rf $CONTAINER_HOME; unzip -q $JBOSS_ZIP -d $WORKSPACE  
+    cp --backup $HUDSONBIN/run-with-pid.sh $CONTAINER_HOME/bin/run.sh
   ;;
   'jboss600')
     SERVER_NAME=default
     JBOSS_BUILD=jboss-6.0.0.M2
     JBOSS_ZIP=$HUDSON_HOME/../jboss/$JBOSS_BUILD.zip
-	RUNTIME_HOME=$WORKSPACE/jboss-6.0.0.20100216-M2
-	RUNTIME_LOG=$RUNTIME_HOME/server/$SERVER_NAME/log/server.log
-    rm -rf $RUNTIME_HOME; unzip -q $JBOSS_ZIP -d $WORKSPACE  
-    cp --backup $HUDSONBIN/run-with-pid.sh $RUNTIME_HOME/bin/run.sh
+	CONTAINER_HOME=$WORKSPACE/jboss-6.0.0.20100216-M2
+	CONTAINER_LOG=$CONTAINER_HOME/server/$SERVER_NAME/log/server.log
+    rm -rf $CONTAINER_HOME; unzip -q $JBOSS_ZIP -d $WORKSPACE  
+    cp --backup $HUDSONBIN/run-with-pid.sh $CONTAINER_HOME/bin/run.sh
   ;;
   'jboss601')
     SERVER_NAME=default
     JBOSS_BUILD=jboss-6.0.0-SNAPSHOT
     JBOSS_ZIP=$HUDSON_HOME/../jboss/$JBOSS_BUILD.zip
-	RUNTIME_HOME=$WORKSPACE/$JBOSS_BUILD
-	RUNTIME_LOG=$RUNTIME_HOME/server/$SERVER_NAME/log/server.log
-    rm -rf $RUNTIME_HOME; unzip -q $JBOSS_ZIP -d $WORKSPACE
-    cp --backup $HUDSONBIN/run-with-pid.sh $RUNTIME_HOME/bin/run.sh
+	CONTAINER_HOME=$WORKSPACE/$JBOSS_BUILD
+	CONTAINER_LOG=$CONTAINER_HOME/server/$SERVER_NAME/log/server.log
+    rm -rf $CONTAINER_HOME; unzip -q $JBOSS_ZIP -d $WORKSPACE
+    cp --backup $HUDSONBIN/run-with-pid.sh $CONTAINER_HOME/bin/run.sh
   ;;
   'runtime')
     SERVER_NAME=all
-	RUNTIME_HOME=$DISTRODIR/auto-install-dest/runtime
-	RUNTIME_LOG=$RUNTIME_HOME/server/$SERVER_NAME/log/server.log
+	CONTAINER_HOME=$DISTRODIR/auto-install-dest/runtime
+	CONTAINER_LOG=$CONTAINER_HOME/server/$SERVER_NAME/log/server.log
   ;;
   *)
 	echo "Unsupported container: $CONTAINER"
@@ -62,6 +62,7 @@ esac
 if [ -f .gitmodules]; then
    git submodule init
    git submodule update
+   git submodule status
 fi
 
 #
@@ -91,7 +92,7 @@ echo $MVN_CMD; $MVN_CMD | tee $WORKSPACE/dependency-tree.txt
 #
 # start jbossas/runtime
 #
-STARTUP_CMD="$HUDSONBIN/startup.sh $RUNTIME_HOME start $SERVER_NAME $JBOSS_BINDADDR"
+STARTUP_CMD="$HUDSONBIN/startup.sh $CONTAINER_HOME start $SERVER_NAME $JBOSS_BINDADDR"
 echo $STARTUP_CMD; $STARTUP_CMD
 
 #
@@ -99,15 +100,15 @@ echo $STARTUP_CMD; $STARTUP_CMD
 #
 $HUDSONBIN/http-spider.sh $JBOSS_BINDADDR:8090/jboss-osgi $WORKSPACE
 if [ -e $WORKSPACE/spider.failed ]; then
-  tail -n 200 $RUNTIME_LOG
-  $HUDSONBIN/startup.sh $RUNTIME_HOME stop
+  tail -n 200 $CONTAINER_LOG
+  $HUDSONBIN/startup.sh $CONTAINER_HOME stop
   exit 1
 fi
 
 #
 # execute tests
 #
-ENVIRONMENT="-Dframework=$FRAMEWORK -Dtarget.container=$CONTAINER -Djboss.home=$RUNTIME_HOME -Djboss.bind.address=$JBOSS_BINDADDR"
+ENVIRONMENT="-Dframework=$FRAMEWORK -Dtarget.container=$CONTAINER -Djboss.home=$CONTAINER_HOME -Djboss.bind.address=$JBOSS_BINDADDR"
 MVN_CMD="mvn -o -Dnoreactor -fae $ENVIRONMENT test"
 echo $MVN_CMD; $MVN_CMD 2>&1 | tee $WORKSPACE/tests.log
 cat $WORKSPACE/tests.log | egrep FIXME\|FAILED | sort -u | tee $WORKSPACE/fixme.txt
@@ -116,4 +117,4 @@ cat $WORKSPACE/fixme.txt | egrep "\[\S*]" > $WORKSPACE/errata-$CONTAINER.txt || 
 #
 # stop jbossas/runtime
 #
-$HUDSONBIN/startup.sh $RUNTIME_HOME stop
+$HUDSONBIN/startup.sh $CONTAINER_HOME stop

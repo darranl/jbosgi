d2071eaee81205efbc7cd4e9ec53b147ac81db15
David Bosschaert <david.bosschaert@gmail.com>
2010-06-24 14:32:15 +0100

    New JMX test case to control start level.

---
+++ testsuite/example/src/test/java/org/jboss/test/osgi/example/jmx/JMXStartLevelTestCase.java
@@ -0,0 +1,116 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2005, JBoss Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.test.osgi.example.jmx;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+
+import java.util.Collection;
+
+import javax.management.openmbean.CompositeData;
+import javax.management.openmbean.TabularData;
+
+import junit.framework.Assert;
+
+import org.jboss.osgi.jmx.JMXCapability;
+import org.jboss.osgi.testing.OSGiBundle;
+import org.jboss.osgi.testing.OSGiRuntime;
+import org.jboss.osgi.testing.OSGiRuntimeHelper;
+import org.jboss.osgi.testing.OSGiRuntimeTest;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.osgi.jmx.framework.BundleStateMBean;
+import org.osgi.jmx.framework.FrameworkMBean;
+
+/**
+ * @author <a href="david@redhat.com">David Bosschaert</a>
+ */
+public class JMXStartLevelTestCase extends OSGiRuntimeTest
+{
+   private static OSGiRuntime runtime;
+
+   @BeforeClass
+   public static void setupClass() throws Exception
+   {
+      runtime = new OSGiRuntimeHelper().getDefaultRuntime();
+      runtime.addCapability(new JMXCapability());
+   }
+
+   @AfterClass
+   public static void tearDownClass() throws Exception
+   {
+      runtime.shutdown();
+      runtime = null;
+   }
+
+   @SuppressWarnings("unchecked")
+   @Test
+   public void testStartLevelMBean() throws Exception
+   {
+      FrameworkMBean fw = runtime.getFrameworkMBean();
+      fw.setInitialBundleStartLevel(2);
+
+      Assert.assertEquals(1, fw.getFrameworkStartLevel());
+      OSGiBundle bundle = runtime.installBundle("example-jmx.jar"); // TODO maybe use another bundle
+
+      BundleStateMBean bs = runtime.getBundleStateMBean();
+      TabularData td = bs.listBundles();
+
+      long bundleId = -1;
+      for (CompositeData row : (Collection<CompositeData>)td.values())
+      {
+         if (bundle.getSymbolicName().equals(row.get("SymbolicName")))
+         {
+            bundleId = Long.parseLong(row.get("Identifier").toString());
+            break;
+         }
+      }
+      assertTrue("Could not find test bundle through JMX", bundleId != -1);
+
+      fw.startBundle(bundleId);
+
+      assertEquals(2, bs.getStartLevel(bundleId));
+      fw.setBundleStartLevel(bundleId, 5);
+      assertEquals(5, bs.getStartLevel(bundleId));
+      waitForBundleState("INSTALLED", bs, bundleId);
+
+      fw.setFrameworkStartLevel(10);
+      waitForBundleState("ACTIVE", bs, bundleId);
+   }
+   
+   private void waitForBundleState(String state, BundleStateMBean bs, long bundleId) throws Exception
+   {
+      int secs = 10;
+      while (secs > 0)
+      {
+         String s = bs.getState(bundleId);
+         if (state.equals(s))
+            return;
+
+         secs--;
+         SECONDS.sleep(1);
+      }
+      Assert.fail("Did not reach bundle state " + state);
+   }
+}

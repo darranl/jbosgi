1f4848244c5b482d307afbcc3495abd33dd3eb91
Thomas Diesler <thomas.diesler@jboss.com>
2010-11-10 21:35:35 +0100

    Add XService examples

---
+++ testsuite/example/src/test/java/org/jboss/test/osgi/example/xservice/BundleAccessesModuleServiceTestCase.java
@@ -0,0 +1,112 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2010, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.test.osgi.example.xservice;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.InputStream;
+
+import org.jboss.modules.ModuleIdentifier;
+import org.jboss.msc.service.ServiceActivator;
+import org.jboss.osgi.testing.OSGiBundle;
+import org.jboss.osgi.testing.OSGiManifestBuilder;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.Asset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.test.osgi.example.xservice.api.Echo;
+import org.jboss.test.osgi.example.xservice.bundle.ClientBundleActivator;
+import org.jboss.test.osgi.example.xservice.module.EchoService;
+import org.jboss.test.osgi.example.xservice.module.TargetModuleActivator;
+import org.junit.Test;
+import org.osgi.framework.Bundle;
+
+/**
+ * A test that shows how a bundle can access an MSC service.
+ *
+ * @author Thomas.Diesler@jboss.com
+ * @since 14-Oct-2010
+ */
+public class BundleAccessesModuleServiceTestCase extends AbstractXServiceTestCase
+{
+   @Test
+   public void testBundleInvokesModuleService() throws Exception
+   {
+      // Deploy the non-OSGi module which contains the target service
+      String targetDeploymentName = getRemoteRuntime().deploy(getTargetModuleArchive());
+      assertNotNull("Deployment name not null", targetDeploymentName);
+      try
+      {
+         // Install the client bundle
+         OSGiBundle clientBundle = getRemoteRuntime().installBundle(getClientBundleArchive());
+         try
+         {
+            assertBundleState(Bundle.INSTALLED, clientBundle.getState());
+
+            // Register the target module with the OSGi layer
+            ModuleIdentifier moduleId = ModuleIdentifier.create("deployment." + targetDeploymentName);
+            registerModuleWithBundleManager(moduleId);
+
+            // Start the client bundle, which calls the target service. Check the console log for echo message
+            clientBundle.start();
+            assertBundleState(Bundle.ACTIVE, clientBundle.getState());
+         }
+         finally
+         {
+            clientBundle.uninstall();
+         }
+      }
+      finally
+      {
+         getRemoteRuntime().undeploy(targetDeploymentName);
+      }
+   }
+
+   private JavaArchive getClientBundleArchive() throws Exception
+   {
+      final JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "example-xservice-client-bundle");
+      archive.addClasses(ClientBundleActivator.class);
+      archive.setManifest(new Asset()
+      {
+         public InputStream openStream()
+         {
+            OSGiManifestBuilder builder = OSGiManifestBuilder.newInstance();
+            builder.addBundleManifestVersion(2);
+            builder.addBundleSymbolicName(archive.getName());
+            builder.addBundleVersion("1.0.0");
+            builder.addBundleActivator(ClientBundleActivator.class);
+            builder.addImportPackages(Echo.class);
+            return builder.openStream();
+         }
+      });
+      return archive;
+   }
+
+   private JavaArchive getTargetModuleArchive() throws Exception
+   {
+      final JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "example-xservice-target-module");
+      archive.addClasses(Echo.class, EchoService.class, TargetModuleActivator.class);
+      String activatorPath = "META-INF/services/" + ServiceActivator.class.getName();
+      archive.addResource(getResourceFile("xservice/target-module/" + activatorPath), activatorPath);
+      return archive;
+   }
+}
---
+++ testsuite/example/src/test/java/org/jboss/test/osgi/example/xservice/ModuleAccessesBundleServiceTestCase.java
@@ -0,0 +1,139 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2010, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.test.osgi.example.xservice;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.InputStream;
+import java.util.jar.JarFile;
+
+import org.jboss.modules.ModuleIdentifier;
+import org.jboss.msc.service.ServiceActivator;
+import org.jboss.msc.service.ServiceController.State;
+import org.jboss.osgi.testing.OSGiBundle;
+import org.jboss.osgi.testing.OSGiManifestBuilder;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.Asset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.test.osgi.example.xservice.api.Echo;
+import org.jboss.test.osgi.example.xservice.api.EchoInvoker;
+import org.jboss.test.osgi.example.xservice.bundle.TargetBundleActivator;
+import org.jboss.test.osgi.example.xservice.module.ClientModuleActivator;
+import org.jboss.test.osgi.example.xservice.module.EchoInvokerService;
+import org.junit.Test;
+import org.osgi.framework.Bundle;
+
+/**
+ * A test that shows how a module can access an OSGi service.
+ *
+ * @author Thomas.Diesler@jboss.com
+ * @since 14-Oct-2010
+ */
+public class ModuleAccessesBundleServiceTestCase extends AbstractXServiceTestCase
+{
+   @Test
+   public void testModuleInvokesTargetBundleService() throws Exception
+   {
+      // Deploy the module that contains the API
+      String apiDeploymentName = getRemoteRuntime().deploy(getAPIModuleArchive());
+      assertNotNull("Deployment name not null", apiDeploymentName);
+      try
+      {
+         // Install the bundle that contains the target service
+         OSGiBundle targetBundle = getRemoteRuntime().installBundle(getTargetBundleArchive());
+         try
+         {
+            assertBundleState(Bundle.INSTALLED, targetBundle.getState());
+
+            // Register the API module with the OSGi layer
+            ModuleIdentifier moduleId = ModuleIdentifier.create("deployment." + apiDeploymentName);
+            registerModuleWithBundleManager(moduleId);
+
+            // Start the target service bundle
+            targetBundle.start();
+            assertBundleState(Bundle.ACTIVE, targetBundle.getState());
+
+            // Deploy the non-osgi client module
+            String clientDeploymentName = getRemoteRuntime().deploy(getClientModuleArchive());
+            assertNotNull("Deployment name not null", clientDeploymentName);
+            try
+            {
+               // Wait for the client service to come up. Check the console log for echo message
+               State state = getServiceState(EchoInvokerService.SERVICE_NAME, 10000);
+               assertEquals("EchoInvokerService is UP", State.UP, state);
+            }
+            finally
+            {
+               getRemoteRuntime().undeploy(clientDeploymentName);
+            }
+         }
+         finally
+         {
+            targetBundle.uninstall();
+         }
+      }
+      finally
+      {
+         getRemoteRuntime().undeploy(apiDeploymentName);
+      }
+   }
+
+   private JavaArchive getAPIModuleArchive() throws Exception
+   {
+      final JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "example-xservice-api-module");
+      archive.addClasses(Echo.class);
+      archive.addDirectory("META-INF");
+      return archive;
+   }
+
+   private JavaArchive getTargetBundleArchive() throws Exception
+   {
+      final JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "example-xservice-target-bundle");
+      archive.addClass(TargetBundleActivator.class);
+      archive.setManifest(new Asset()
+      {
+         public InputStream openStream()
+         {
+            OSGiManifestBuilder builder = OSGiManifestBuilder.newInstance();
+            builder.addBundleManifestVersion(2);
+            builder.addBundleSymbolicName(archive.getName());
+            builder.addBundleVersion("1.0.0");
+            builder.addBundleActivator(TargetBundleActivator.class);
+            builder.addImportPackages(Echo.class);
+            return builder.openStream();
+         }
+      });
+      return archive;
+   }
+
+   private JavaArchive getClientModuleArchive() throws Exception
+   {
+      final JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "example-xservice-client-module");
+      archive.addClasses(EchoInvoker.class, EchoInvokerService.class, ClientModuleActivator.class);
+      String activatorPath = "META-INF/services/" + ServiceActivator.class.getName();
+      archive.addResource(getResourceFile("xservice/client-module/" + activatorPath), activatorPath);
+      archive.setManifest(getResourceFile("xservice/client-module/" + JarFile.MANIFEST_NAME));
+      return archive;
+   }
+}

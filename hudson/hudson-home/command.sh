4c7070f90601e6115f39ac611496d023e691ab06
Thomas Diesler <thomas.diesler@jboss.com>
2011-03-16 08:58:53 +0100

    Prepare remote testrun agains jbossas-1.0.0.Beta1

--- hudson/hudson-home/command.sh
+++ hudson/hudson-home/command.sh
@@ -11,16 +11,14 @@ HUDSONBIN=$HUDSONDIR/hudson-home/bin
 
 case "$CONTAINER" in
   'jboss700')
-    SERVER_NAME=default
-    JBOSS_BUILD=jboss-7.0.0.Alpha1
+    JBOSS_BUILD=jboss-7.0.0.Beta1
     JBOSS_ZIP=$HUDSON_HOME/userContent/$JBOSS_BUILD.zip
     CONTAINER_HOME=$WORKSPACE/$JBOSS_BUILD
     CONTAINER_LOG=$CONTAINER_HOME/standalone/log/boot.log
     rm -rf $CONTAINER_HOME; unzip -q $JBOSS_ZIP -d $WORKSPACE
   ;;
   'jboss70x')
-    SERVER_NAME=default
-    JBOSS_BUILD=jboss-7.0.0.Alpha2
+    JBOSS_BUILD=jboss-7.0.0.Beta2
     JBOSS_ZIP=$HUDSON_HOME/userContent/$JBOSS_BUILD-SNAPSHOT.zip
     CONTAINER_HOME=$WORKSPACE/$JBOSS_BUILD
     CONTAINER_LOG=$CONTAINER_HOME/standalone/log/boot.log

73baaddeab46ed0f8023cd0b05e8524671dcd8c3
Thomas Diesler <thomas.diesler@jboss.com>
2011-10-24 11:43:16 +0200

    Update to framework-1.0.1

--- testsuite/jbossas/deploy.sh
+++ testsuite/jbossas/deploy.sh
@@ -16,7 +16,8 @@ fi
 # Find and deploy the target files
 for TARGET in "api" "service"  "ejb3" "webapp"
 do
-	TARGET_FILE="`find $TARGET -type f -name jboss-osgi-example-jbossas* | grep -v sources`"
+	TARGET_FILE="`find $TARGET -type f -name 'jboss-osgi-example-jbossas-*' | grep -v sources | grep -v iml`"
 	echo "Deploying $TARGET_FILE"
 	cp $TARGET_FILE $DEPLOYMENTS_DIR
+    sleep 1s
 done 
